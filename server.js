fe8ced0da2bc75c48281649ee19f605fc295c20e
MestiriJacem <mestirijacemm@gmail.com>
2025-02-15 09:53:25 +0100

    server dtaabse connection models

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const  cors = require('cors')
+const mongoose =require('mongoose');
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors());
+app.use(express.json())
+
+
+//database connection
+mongoose.connect(process.env.MONGO_URI ,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>console.log('database connected'))
+.catch (err=>console.error ('mongo connection error',err));
+//Port
+const PORT= process.env.PORT || 5000;
+app.listen (PORT,()=>console.log(`server running on port ${PORT}`))
